b31ae388b615cc6ebb97be9b4a3f3d80ba3114bf
Casey <hjkdw9506@gmail.com>
2022-05-22 12:59:26 +0900

    Commit6

--- src/components/Nav/NavTest1.js
+++ src/components/Nav/NavTest1.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const NavTest1 = props => <div>another test for merge 2</div>;
+const NavTest1 = props => (
+  <>
+    <div>another test for merge 2</div>
+    <div>another test for merge 3</div>
+  </>
+);
 
 export default NavTest1;
