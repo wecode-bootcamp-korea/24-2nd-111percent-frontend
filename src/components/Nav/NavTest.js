924c69c9b15759c8d645ca82ee96fdd321737f54
Casey <hjkdw9506@gmail.com>
2022-05-22 12:53:23 +0900

    Commit2

--- src/components/Nav/NavTest.js
+++ src/components/Nav/NavTest.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const NavTest = props => <div>this is just for test</div>;
+const NavTest = props => (
+  <>
+    <div>this is just for test</div>
+    <div>this is for second test</div>
+  </>
+);
 
 export default NavTest;
